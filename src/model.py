eb6f5f19361910fe6289e044b9efc2d6a56dbcf6
Tim Whitaker <tmwhtkr@gmail.com>
2018-10-02 08:35:35 -0600

    Add model

--- src/model.py
+++ src/model.py
@@ -1,3 +1,5 @@
+
+# Transitions the agent observes
 Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
 
 class ReplayMemory(object):
@@ -11,7 +13,19 @@ class ReplayMemory(object):
 
 class DQN(nn.module):
 	def __init__(self):
+		super(DQN, self).__init__()
+		self.conv1 = nn.Conv2d(3, 16, kernel_size=5, stride=2)
+		self.bn1 = nn.BatchNorm2d(16)
+		self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
+		self.bn2 = nn.BatchNorm2d(32)
+		self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
+		self.bn3 = nn.BatchNorm2d(32)
+		self.head == nn.Linear(448, 2)
 
 	def forward(self, x):
+		x = F.relu(self.bn1(self.conv1(x)))
+		x = F.relu(self.bn2(self.conv2(x)))
+		x = F.relu(self.bn3(self.conv3(x)))
+		return self.head(x.view(x.size(0), -1))
 
 def select_action():
