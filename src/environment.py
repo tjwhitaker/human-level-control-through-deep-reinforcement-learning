52b85eb8943ff421908e53d1fc5aa1aa0b32005b
Tim Whitaker <tmwhtkr@gmail.com>
2018-10-02 09:00:44 -0600

    Add environment functions

--- src/environment.py
+++ src/environment.py
@@ -1,3 +1,25 @@
-def get_cart_location():
+def init():
+	return gym.make('CartPole-v0').unwrapped
 
-def get_screen():
+def get_cart_location(world, screen_width):
+	world_width = world.x_threshold * 2
+	scale = screen_width / world_width
+	return int(world.state[0] * scale + screen_width / 2.0)
+
+def get_screen(world, screen_width, device):
+	screen = world.render(mode='rgb_array').transpose((2, 0, 1))
+	screen = screen[:, 160:320]
+	view_width = 320
+	cart_location = get_cart_location(world, screen_width)
+
+	if cart_location < view_width // 2:
+		slice_range = slice(view_width)
+	elif cart_location > (screen_width - view_width // 2):
+		slice_range = slice(-view_width, None)
+	else:
+		slice_range = slice(cart_location - view_width // 2, cart_location + view_width // 2)
+
+	screen = screen[:, :, slice_range]
+	screen = np.ascontiguousarray(screen, dtype=np.float32) / 255
+	screen = torch.from_numpy(screen)
+	return resize(screen).unsqueeze(0).to(device)
--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
 import torch
 
 # Config
-device = torch.device('cuda')
-
-world = gym.make('CartPole-v0').unwrapped
 screen_width = 600
-
 batch_size = 128
 gamma = 0.999
-eps_start = 0.9
-eps_end = 0.05
-eps_decay = 200
+epsilon_start = 0.9
+epsilon_end = 0.05
+epsilon_decay = 200
 target_decay = 10
+
+device = torch.device('cuda')
+
+world = environment.init()
