7ae05997d914f20fad39e85a2198835a5f753ef3
Tim Whitaker <tmwhtkr@gmail.com>
2018-10-02 08:48:12 -0600

    Add replay memory

--- src/model.py
+++ src/model.py
@@ -1,15 +1,18 @@
-
-# Transitions the agent observes
 Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
 
 class ReplayMemory(object):
 	def __init__(self, capacity):
+		self.capacity = capacity
+		self.memory = deque(maxlen=capacity)
 
 	def push(self, *args):
+		self.memory.append(Transition(*args))
 
 	def sample(self, batch_size):
+		return random.sample(self.memory, batch_size)
 
 	def __len__(self):
+		return len(self.memory)
 
 class DQN(nn.module):
 	def __init__(self):
